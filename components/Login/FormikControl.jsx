e69a51339109ca030126c718d97284a7120e6671
mohammadmd9776 <mohammadmd9776@gmail.com>
2021-12-29 20:57:29 +0330

    lastFormated

--- components/Login/FormikControl.jsx
+++ components/Login/FormikControl.jsx
@@ -6,11 +6,7 @@ function FormikControl(props) {
   switch (control) {
     case "input":
       return <Input {...rest} />;
-    case "textarea":
-    case "select":
-    case "radio":
-    case "checkbox":
-    case "date":
+
     default:
       return null;
   }
--- components/Login/Input.jsx
+++ components/Login/Input.jsx
@@ -3,7 +3,7 @@ import { Field, ErrorMessage } from "formik";
 import TextError from "./TextError";
 import { TextField } from "@material-ui/core";
 
-import Fieldstyle from "./Fieldstyle";
+
 
 const Input = (props) => {
   const { label, name, ...rest } = props;
