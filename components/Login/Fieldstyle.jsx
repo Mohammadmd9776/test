7d0f27a1ac52c7ccf5c1ef6fc87a7cc32bf41bb1
mohammadmd9776 <mohammadmd9776@gmail.com>
2021-12-29 20:28:49 +0330

    reformated

---
+++ components/Login/Fieldstyle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { TextField } from "@material-ui/core";
+
+const Fieldstyle = () => {
+  return <div >
+      <TextField > </TextField>
+      
+  </div>;
+};
+
+export default Fieldstyle;
--- components/Login/Input.jsx
+++ components/Login/Input.jsx
@@ -3,15 +3,33 @@ import { Field, ErrorMessage } from "formik";
 import TextError from "./TextError";
 import { TextField } from "@material-ui/core";
 
+import Fieldstyle from "./Fieldstyle";
+
 const Input = (props) => {
   const { label, name, ...rest } = props;
   return (
-    <div style={{ marginBottom: 1 }}>
+    <div >
       <div>
         <label htmlFor={name}>{label}</label>
       </div>
 
-      <Field id={name} name={name} {...rest} />
+      
+
+      <Field id={name} name={name} {...rest} 
+      render={({ field /* { name, value, onChange, onBlur } */ }) => (
+        
+        <TextField  {...field}/>
+
+        
+      )}
+      
+      
+      
+      
+      />
+
+      
+      
 
       <ErrorMessage name={name} component={TextError} />
     </div>
